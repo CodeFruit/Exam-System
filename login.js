9911cadecc8c149611fd7e345f2112412cee0f11
CodeFruit <mauri.patino@gmail.com>
2020-06-25 17:38:41 -0400

    fixed error

--- login.js
+++ login.js
@@ -1,20 +1,14 @@
-var ProjectInput = (function () {
+var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
         this.templateElement = document.getElementById('project-input');
-        !as;
-        HTMLTemplateElement;
-        this.hostElement = document.getElementById('app');
-        !as;
-        HTMLDivElement; //holds a reference to the element where I want to render my template content
+        this.hostElement = document.getElementById('app'); //holds a reference to the element where I want to render my template content
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
-        as;
-        HTMLFormElement;
         this.attach();
     }
     ProjectInput.prototype.attach = function () {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     };
     return ProjectInput;
-})();
+}());
 var test = new ProjectInput();
--- login.ts
+++ login.ts
@@ -4,11 +4,11 @@ class ProjectInput {
     element: HTMLFormElement;
 
     constructor() { //gets access to template and form
-        this.templateElement =  document.getElementById('project-input')! as HTMLTemplateElement;
-        this.hostElement = document.getElementById('app')! as HTMLDivElement;  //holds a reference to the element where I want to render my template content
+        this.templateElement = <HTMLTemplateElement> document.getElementById('project-input')!;
+        this.hostElement = <HTMLDivElement> document.getElementById('app')! ;  //holds a reference to the element where I want to render my template content
 
         const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element = <HTMLFormElement> importedNode.firstElementChild ;
         this.attach();
     }
 
