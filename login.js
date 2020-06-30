bd06989d2184056603754956a6455259650bf8ce
CodeFruit <mauri.patino@gmail.com>
2020-06-30 13:01:56 -0400

    updated

--- login.js
+++ login.js
@@ -1,11 +1,21 @@
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
-        this.templateElement = document.getElementById('project-input');
+        this.templateElement = document.getElementById('project');
         this.hostElement = document.getElementById('app'); //holds a reference to the element where I want to render my template content
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.userInput = this.element.querySelector('#user');
+        this.passInput = this.element.querySelector('#pass');
+        this.configure();
         this.attach();
     }
+    ProjectInput.prototype.submitHandler = function (event) {
+        event.preventDefault();
+        console.log(this.userInput.value);
+    };
+    ProjectInput.prototype.configure = function () {
+        this.element.addEventListener('Check', this.submitHandler.bind(this));
+    };
     ProjectInput.prototype.attach = function () {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     };
--- login.ts
+++ login.ts
@@ -2,14 +2,31 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    userInput: HTMLInputElement;
+    passInput: HTMLInputElement;
 
     constructor() { //gets access to template and form
-        this.templateElement = <HTMLTemplateElement> document.getElementById('project-input')!;
+        this.templateElement = <HTMLTemplateElement> document.getElementById('project')!;
         this.hostElement = <HTMLDivElement> document.getElementById('app')! ;  //holds a reference to the element where I want to render my template content
 
         const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = <HTMLFormElement> importedNode.firstElementChild ;
+        this.element = <HTMLFormElement> importedNode.firstElementChild;
+
+        this.userInput = <HTMLInputElement> this.element.querySelector('#user');
+        this.passInput = <HTMLInputElement> this.element.querySelector('#pass');
+
+
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.userInput.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('Check', this.submitHandler.bind(this));
     }
 
     private attach(){
